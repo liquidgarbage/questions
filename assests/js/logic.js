bd09df762f391cdbe1048a5ca81a274a5dc31caa
Milo <128057494+liquidgarbage@users.noreply.github.com>
2024-04-02 14:48:01 +0000

    1 js file testing

--- assests/js/logic.js
+++ assests/js/logic.js
@@ -1,1 +1,20 @@
 //question pops up with button next question then new question from the questionsArray pops up, selects random question
+
+let questionsArray = [
+    'what is your favouite kind of pen?',
+    'fave biscuit?',
+    'Best hot drink?',
+    'When did you first get drunk?',
+    'Herbal tea?',
+]
+
+console.log(questionsArray)
+
+function 
+
+//mathrandom to generate array
+//how to show array in html section
+
+document.getElementsByClassName('qBox').innerHTML
+
+//on click next button math random in array
