a8abd77baec0e2747eb2931fa7f658aea799e778
Milo <128057494+liquidgarbage@users.noreply.github.com>
2024-04-06 13:51:55 +0000

    it showed the question

--- assests/js/logic.js
+++ assests/js/logic.js
@@ -1,12 +1,12 @@
 
 console.log(questionsArray)
 
-let question = questionsArray.toString();
-document.getElementsByClassName("qBox").innerHTML = question
+let question = questionsArray[Math.floor(Math.random()*questionsArray.length)];
+document.getElementById('HTMLquestion').innerHTML = question;
+
 //mathrandom to generate array
 //how to show array in html section
 
-// document.getElementsByClassName('qBox').innerHTML
 //question pops up with button next question then new question from the questionsArray pops up, selects random question
 
 //on click next button math random in array
