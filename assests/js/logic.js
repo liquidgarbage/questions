3a0242d21ad5ef2f84133c0819e0a1bd19c95056
liquidgarbage <128057494+liquidgarbage@users.noreply.github.com>
2024-04-08 17:13:32 +0100

    button works

--- assests/js/logic.js
+++ assests/js/logic.js
@@ -1,20 +1,6 @@
-
-console.log(questionsArray)
-
-let question = questionsArray[Math.floor(Math.random() * questionsArray.length)];
-document.getElementById('HTMLquestion').innerHTML = question;
-
-//mathrandom to generate array
-//how to show array in html section
-
-//question pops up with button next question then new question from the questionsArray pops up, selects random question
-
-//on click next button math random in array
-//addEventListener
 let nextButton = document.getElementById('nextB');
 
 nextButton.addEventListener('click', function () {
-    console.log(question)
-    questionsArray[Math.floor(Math.random() * questionsArray.length)];
-    //splice new q in to old one
+    question = questionsArray[Math.floor(Math.random() * questionsArray.length)];
+    document.getElementById('HTMLquestion').innerHTML = question;
 })
