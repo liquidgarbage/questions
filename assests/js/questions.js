930ce38f3b2631bc2f1d2aadc89816f8bda368ab
Milo <128057494+liquidgarbage@users.noreply.github.com>
2024-05-21 14:14:29 +0000

    comment out css

--- assests/js/questions.js
+++ assests/js/questions.js
@@ -1,7 +1,8 @@
 let questionsArray = [
-        'what is your favouite kind of pen?',
-        'fave biscuit?',
+        'What is your favouite kind of pen?',
+        'Fave biscuit?',
         'Best hot drink?',
         'When did you first get drunk?',
         'Opinion of herbal tea?',
+
     ]
