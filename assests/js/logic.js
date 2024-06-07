895b7ddda6253118b6a79cd4e147504bce4b4028
liquidgarbage <128057494+liquidgarbage@users.noreply.github.com>
2024-06-07 15:24:38 +0100

    spelling mistake

--- assests/js/logic.js
+++ assests/js/logic.js
@@ -4,3 +4,13 @@ nextButton.addEventListener('click', function () {
     question = questionsArray[Math.floor(Math.random() * questionsArray.length)];
     document.getElementById('HTMLquestion').innerHTML = question;
 })
+
+
+// let prevButton = document.getElementById('prevB');
+// prevButton.addEventListener('click', function () {
+//     // Retrieve the previously selected question
+//     let previousQuestion = document.getElementById('HTMLquestion').innerHTML;
+//     question = previousQuestion;
+//     document.getElementById('HTMLquestion').innerHTML = question;
+
+// })
