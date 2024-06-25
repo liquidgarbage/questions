224b9b8b49d6fd61ca6356ead2380c2429fb122d
liquidgarbage <128057494+liquidgarbage@users.noreply.github.com>
2024-06-25 15:08:46 +0100

    prev q goes back 1

--- assests/js/logic.js
+++ assests/js/logic.js
@@ -1,21 +1,18 @@
-let nextButton = document.getElementById('nextB');
-let prevButton = document.getElementById('prevQ');
-let visibleQuestion = document.getElementById('HTMLquestion')
-let questionHistory = []
-
+let nextButton = document.getElementById("nextB");
+let prevButton = document.getElementById("prevQ");
+let visibleQuestion = document.getElementById("HTMLquestion");
+let questionHistory = [];
 
 function nextQ() {
-    question = questionsArray[Math.floor(Math.random() * questionsArray.length)];
-    visibleQuestion.textContent = question;
-    //questions added to questionHistory array 
+  question = questionsArray[Math.floor(Math.random() * questionsArray.length)];
+  visibleQuestion.textContent = question;
+  questionHistory.push(question);
+  //questions added to questionHistory array
 }
-
 
 function prevQ() {
-//gets q from questionHistory 
-//in reverse order
-//shows on visibleQuestion
+  oldQuestion = questionHistory.at(-2)  
+  visibleQuestion.textContent = oldQuestion
 }
 
-
-//start button with description in <p> hide next and previous, show next and previous when pressed start and hide start.  
+//start button with description in <p> hide next and previous, show next and previous when pressed start and hide start.
