16c898abdfc9193f3321e7e2debb625fb58f97ad
liquidgarbage <128057494+liquidgarbage@users.noreply.github.com>
2024-06-25 15:20:20 +0100

    startbutton comment

--- assests/js/logic.js
+++ assests/js/logic.js
@@ -7,12 +7,11 @@ function nextQ() {
   question = questionsArray[Math.floor(Math.random() * questionsArray.length)];
   visibleQuestion.textContent = question;
   questionHistory.push(question);
-  //questions added to questionHistory array
 }
 
 function prevQ() {
-  oldQuestion = questionHistory.at(-2)  
-  visibleQuestion.textContent = oldQuestion
+  oldQuestion = questionHistory.at(-2);
+  visibleQuestion.textContent = oldQuestion;
 }
 
 //start button with description in <p> hide next and previous, show next and previous when pressed start and hide start.
